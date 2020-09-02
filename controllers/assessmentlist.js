81f57511dbf927bc4b6b6f8e42727d2eb640ae6f
Glitch (clover-meadow-principal) <none>
2020-09-02 19:34:35 +0000

    🏅📟 Checkpoint ./controllers/assessmentlist.js:18261450/7

--- controllers/assessmentlist.js
+++ controllers/assessmentlist.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const logger = require('../utils/logger');
-const assessmentCollection = require('../models/assessment-store.js');
+const assessmentCollection = require("../models/assessment-store.js");
 
 const assessmentlist = {
   index(request, response) {
     const viewData = {
-      title: 'Assessments',
+      title: 'Assessment',
     };
     response.render('assessmentlist', viewData);
   },
