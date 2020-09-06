e04d0fc7574012e7200edab48c56777344f7a517
Glitch (clover-meadow-principal) <none>
2020-09-06 15:13:44 +0100

    Trying to bring into Glitch

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,18 +1,43 @@
 "use strict";
 
-const logger = require("../utils/logger");
-const assessmentCollection= require("../models/assessment-store.js");
+const logger = require('../utils/logger');
+const assessmentStore = require('../models/assessment-store.js');
+const uuid = require('uuid');
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
-      title: "Dashboard",
-      assessments: assessmentCollection
+      title: 'Dashboard',
+      assessments: assessmentStore.getAllAssessments()
     };
-    logger.info("about to render", assessmentCollection);
+    logger.info('about to render', assessmentStore.getAllAssessments());
     response.render("dashboard", viewData);
+  },
+  deleteAssessmentlist(request, response) {
+    const assessmentlistId = request.params.id;
+    logger.debug(`Deleting Assessmentlist ${assessmentlistId}`);
+    assessmentStore.removeAssessmentlist(assessmentlistId);
+    response.redirect('/dashboard');
+  },
+
+
+  addAssessmentlist(request, response) {
+    const newAssessmentList = {
+      id: uuid.v1(),
+      memberName: request.body.memberName,
+      memberHeight: request.body.memberHeight,
+      memberStartWeight: request.body.memberStartWeight,
+      BMI: request.body.BMI,
+      assessmentCount: request.body.assessmentCount,
+      assessments: [],
+    };
+    assessmentStore.addAssessmentlist(newAssessmentList);
+    response.redirect('/dashboard')
   }
+
+
+
 };
 
 module.exports = dashboard;
--- routes.js
+++ routes.js
@@ -2,16 +2,25 @@
 
 const express = require("express");
 const router = express.Router();
-const assessmentlist = require("./controllers/assessmentlist.js");
+
 
 
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
+const assessmentlist = require('./controllers/assessmentlist.js');
+
+
+
 
 
 router.get("/", dashboard.index);
 router.get("/dashboard", dashboard.index);
+router.get('/dashboard/deleteassessmentlist/:id', dashboard.deleteAssessmentlist);
+router.post('/dashboard/addassessmentlist', dashboard.addAssessmentlist);
+
 router.get("/about", about.index);
-router.get("/assessmemtlist/:id", assessmentlist.index)
+router.get('/assessmentlist/:id', assessmentlist.index);
+router.get('/assessmentlist/:id/deleteassessment/:assessmentid', assessmentlist.deleteAssessment);
+router.post('/assessmentlist/:id/addassessments', assessmentlist.addAssessment);
 
 module.exports = router;
